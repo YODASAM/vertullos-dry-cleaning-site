2ac694b5a0283287c74929ce7ac2c80eeb298ed4
YODASAM <jsampathkumar@gmail.com>
2025-11-12 22:39:23 +0530

    Delete jsx.js

--- components/jsx.js
+++
@@ -1,6 +0,0 @@
-// components/JSX.js
-import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
-
-export const jsx = _jsx;
-export const jsxs = _jsxs;
-export const Fragment = _Fragment;
